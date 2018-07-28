934921da15fcabfccbc97711ce6a6ba8f5895ea9
Chris Nantau <Chris.Nantau@gmail.com>
2018-07-28 18:51:43 -0300

    Extension added for interface types to construct without using the constructor

---
+++ wimm.Guards.Types.Tests/ExtensionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace wimm.Guards.Types.Tests
+{
+    public class ExtensionTests
+    {
+        [Fact]
+        public void ToDefinitely_NullObject_Throws()
+        {
+            var input = null as IEnumerable<string>;
+            Assert.Throws<ArgumentNullException>(() => input.ToDefinitely());
+        }
+
+        [Fact]
+        public void ToDefinitely_ValidInterface_ReturnValueRefEqualsInput()
+        {
+            var input = new List<string> { "Doot" } as IEnumerable<string>;
+            var res = input.ToDefinitely();
+
+            Assert.Same(input, res.Value);
+        }
+
+        [Fact]
+        public void ToDefinitely_ValidClass_ReturnValueRefEqualsInput()
+        {
+            var input = new List<string> { "DootDoot " };
+            var res = input.ToDefinitely();
+
+            Assert.Same(input, res.Value);
+        }
+
+    }
+
+
+}
---
+++ wimm.Guards.Types/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wimm.Guards.Types
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Method for converting types into definitely types. Mostly for interfaces.
+        /// </summary>
+        /// <remarks> Generally this should only be necessary for interface classes to ensure they aren't null
+        /// the C# language doesn't allow implicit conversions to or from interfaces other than up the 
+        /// inheritance heirarchy and we don't want to have to expose a public constructor (for now). 
+        /// [Conditional](PublicConstructor)
+        /// </remarks>
+        /// <returns> A <see cref="Definitely{T}"/> containing the object <paramref name="toConvert"/> </returns>
+        public static Definitely<T> ToDefinitely<T>(this T toConvert) where T : class
+        {
+            return new Definitely<T>(toConvert);
+        }
+    }
+}
